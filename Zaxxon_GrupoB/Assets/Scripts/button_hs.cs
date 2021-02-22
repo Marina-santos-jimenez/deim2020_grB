8e0624ee200084378e3a1da0f85f0830d45dbd78
Marina-santos-jimenez <72887273+Marina-santos-jimenez@users.noreply.github.com>
2021-02-22 14:18:48 +0100

    ui completa

---
+++ Zaxxon_GrupoB/Assets/Scripts/button_hs.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class button_hs : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Hs()
+    {
+       SceneManager.LoadScene(3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
